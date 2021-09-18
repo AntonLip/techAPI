a9903b68af0209d86cdd20e9b33e267fc9d51286
AntonLip <liplianin.anton@yandex.ru>
2021-09-18 23:18:30 +0300

    Add unit tests

--- techAPI.Models/SettingClasses/MapperProfile.cs
+++ techAPI.Models/SettingClasses/MapperProfile.cs
@@ -5,14 +5,17 @@ using techAPI.Models.ModelsDto;
 
 namespace techAPI.Models.SettingClass
 {
-    class MapperProfile : Profile
+    public class MapperProfile : Profile
     {
         public MapperProfile()
         {
             CreateMap<AddDeviceDto, Device>().AfterMap((src, dest) => dest.Id = Guid.NewGuid());
 
             CreateMap<UpdateDeviceDto, Device>().ReverseMap();
-            
+
+            CreateMap<UpdateDeviceDto, DeviceDto>().ReverseMap();
+            CreateMap<DeviceDto, AddDeviceDto>().ReverseMap();
+
             CreateMap<DeviceDto, Device>().ReverseMap();
 
         }
